7332b9e8498b0feaf21d66a888d175064b05358c
Bibek Karki <bibekk477@gmail.com>
2025-11-21 13:30:41 +0545

    removed venv activation from entrypoint

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,7 +1,3 @@
-# Activate the virtual environment
-Write-Host "Activating virtual environment..."
-& .\venv\Scripts\Activate.ps1
-
 # Start FastAPI
 Write-Host "Starting FastAPI..."
 Start-Process uvicorn "fast_api_inference.main:app --host 0.0.0.0 --port 8000 --reload"
