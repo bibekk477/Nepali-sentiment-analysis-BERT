b894d52cc83bb8206b23fac1a18e52031579712e
Bibek Karki <bibekk477@gmail.com>
2025-11-21 12:33:11 +0545

    Added activation of venv in entrypoint.ps1 before starting fastapi and gradio

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,5 +1,11 @@
-Write-Host "Starting FastAPI..."
-uvicorn fast_api_inference.main:app --host 0.0.0.0 --port 8000 --reload
+# Activate the virtual environment
+Write-Host "Activating virtual environment..."
+& .\venv\Scripts\Activate.ps1
 
+# Start FastAPI
+Write-Host "Starting FastAPI..."
+Start-Process uvicorn "fast_api_inference.main:app --host 0.0.0.0 --port 8000 --reload"
+
+# Start Gradio
 Write-Host "Starting Gradio..."
-Start-Process python "gradio_app.py"
+python gradio_app.py
